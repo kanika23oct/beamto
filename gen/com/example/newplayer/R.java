c75f820315cab84827aa332af3f52a29c79c4738
kanika23oct <devnull@localhost>
2013-06-16 23:04:26 -0700

    R.java is updated

--- gen/com/example/newplayer/R.java
+++ gen/com/example/newplayer/R.java
@@ -64,36 +64,41 @@ public final class R {
         public static final int track_info_bg=0x7f020028;
     }
     public static final class id {
-        public static final int action_settings=0x7f080013;
-        public static final int albumTitle=0x7f080010;
-        public static final int btnAlbumPlay=0x7f080011;
-        public static final int btnBackward=0x7f08000c;
-        public static final int btnForward=0x7f08000e;
-        public static final int btnNext=0x7f08000f;
-        public static final int btnPlay=0x7f08000d;
-        public static final int btnPlaylist=0x7f080002;
-        public static final int btnPrevious=0x7f08000b;
-        public static final int btnRepeat=0x7f080004;
-        public static final int btnShuffle=0x7f080005;
-        public static final int btnSongPlay=0x7f080012;
-        public static final int player_footer_bg=0x7f08000a;
-        public static final int player_header_bg=0x7f080000;
-        public static final int songCurrentDurationLabel=0x7f080008;
-        public static final int songProgressBar=0x7f080006;
-        public static final int songThumbnail=0x7f080003;
-        public static final int songTitle=0x7f080001;
-        public static final int songTotalDurationLabel=0x7f080009;
-        public static final int timerDisplay=0x7f080007;
+        public static final int action_settings=0x7f080016;
+        public static final int albumTitle=0x7f080013;
+        public static final int btnAlbumPlay=0x7f080014;
+        public static final int btnBackward=0x7f08000f;
+        public static final int btnForward=0x7f080011;
+        public static final int btnNext=0x7f080012;
+        public static final int btnPlay=0x7f080010;
+        public static final int btnPlaylist=0x7f080005;
+        public static final int btnPrevious=0x7f08000e;
+        public static final int btnRepeat=0x7f080007;
+        public static final int btnShuffle=0x7f080008;
+        public static final int btnSongPlay=0x7f080015;
+        public static final int grid_view=0x7f080000;
+        public static final int icon_image=0x7f080001;
+        public static final int icon_text=0x7f080002;
+        public static final int player_footer_bg=0x7f08000d;
+        public static final int player_header_bg=0x7f080003;
+        public static final int songCurrentDurationLabel=0x7f08000b;
+        public static final int songProgressBar=0x7f080009;
+        public static final int songThumbnail=0x7f080006;
+        public static final int songTitle=0x7f080004;
+        public static final int songTotalDurationLabel=0x7f08000c;
+        public static final int timerDisplay=0x7f08000a;
     }
     public static final class layout {
         public static final int bg_player_footer=0x7f030000;
         public static final int bg_player_header=0x7f030001;
-        public static final int player=0x7f030002;
-        public static final int playlist=0x7f030003;
-        public static final int playlist_item=0x7f030004;
-        public static final int rounded_corner=0x7f030005;
-        public static final int songlist=0x7f030006;
-        public static final int songlist_item=0x7f030007;
+        public static final int grid_layout=0x7f030002;
+        public static final int gridlayout_item=0x7f030003;
+        public static final int player=0x7f030004;
+        public static final int playlist=0x7f030005;
+        public static final int playlist_item=0x7f030006;
+        public static final int rounded_corner=0x7f030007;
+        public static final int songlist=0x7f030008;
+        public static final int songlist_item=0x7f030009;
     }
     public static final class menu {
         public static final int media_player=0x7f070000;
