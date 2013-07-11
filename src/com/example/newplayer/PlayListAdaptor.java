89eec30a3fa3efa7438c2ebaca700b220d2ef0ee
kanika23oct <devnull@localhost>
2013-07-10 20:20:25 -0700

    Adding the logic to display play list and change the songs

---
+++ src/com/example/newplayer/PlayListAdaptor.java
@@ -0,0 +1,69 @@
+package com.example.newplayer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class PlayListAdaptor extends BaseAdapter {
+
+	ArrayList<HashMap<String, String>> songsList;
+	private Context context;
+	private LayoutInflater inflater;
+
+	public PlayListAdaptor(Context context,
+			ArrayList<HashMap<String, String>> songList) {
+		inflater = LayoutInflater.from(context);
+		this.context = context;
+		this.songsList = songList;
+	}
+
+	@Override
+	public int getCount() {
+		return songsList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return songsList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		final HashMap<String, String> songs = songsList.get(position);
+		View v = null;
+		if (convertView != null)
+			v = convertView;
+		else
+			v = inflater.inflate(R.layout.playlist_item, parent, false);
+		
+		final String songName = songs.get("songName");
+		
+		TextView itemSongName = (TextView) v.findViewById(R.id.albumTitle);
+		itemSongName.setText(songName);
+		itemSongName.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String albumName = songs.get("albumName");
+				String url = songs.get("songUrl");
+				NewMediaPlayer.playSong(url);
+				NewMediaPlayer.songTitleLabel.setText(albumName
+						+ " - " + songName);
+				 NewMediaPlayer.songTitle.setText(albumName+"-"+songName);
+			}
+		});
+		return v;
+	}
+
+}
