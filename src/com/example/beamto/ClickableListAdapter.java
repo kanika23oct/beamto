cc333b8cf00e79deb9edcfa38ada56577e30fd24
kanika23oct <devnull@localhost>
2013-08-14 22:13:52 -0700

    changed the  songs list to be fetched and started at background

--- src/com/example/beamto/ClickableListAdapter.java
+++ src/com/example/beamto/ClickableListAdapter.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import com.example.newplayer.R;
+import com.example.beamto.R;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
@@ -22,8 +22,6 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-
-
 public class ClickableListAdapter extends BaseAdapter {
 
 	private Context context;
@@ -31,13 +29,12 @@ public class ClickableListAdapter extends BaseAdapter {
 	List<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
 	String albumName = "";
 	private static Resources resources;
-	StringBuffer albumUrl;
+	String albumUrl;
 	private String imageURL = "";
 	ImageView imageView;
-	public static HashMap<String,Bitmap> imageMap = new HashMap<String,Bitmap>();
+	public static HashMap<String, Bitmap> imageMap = new HashMap<String, Bitmap>();
 	DisplayImageOptions options;
 	ImageLoader imageLoader;
-	
 
 	@SuppressWarnings("deprecation")
 	public ClickableListAdapter(Context context,
@@ -46,30 +43,28 @@ public class ClickableListAdapter extends BaseAdapter {
 		this.context = context;
 		this.songsList = songList;
 		resources = context.getResources();
-		imageLoader= ImageLoader.getInstance();
+		imageLoader = ImageLoader.getInstance();
 		imageLoader.init(ImageLoaderConfiguration.createDefault(context));
-		options = new DisplayImageOptions.Builder().showStubImage(R.drawable.ic_launcher)
-			    .showImageForEmptyUri(R.drawable.ic_launcher)
-			    .cacheOnDisc()
-			    .cacheInMemory()
-			    .build();
-		
-		
+		options = new DisplayImageOptions.Builder()
+				.showStubImage(R.drawable.ic_launcher)
+				.showImageForEmptyUri(R.drawable.ic_launcher).cacheOnDisc()
+				.cacheInMemory().build();
+
 	}
 
 	public void addToList(HashMap<String, String> song) {
 		songsList.add(song);
 	}
-	
-	public void setImage(String url,Bitmap image) {
+
+	public void setImage(String url, Bitmap image) {
 		imageMap.put(url, image);
 	}
-	
+
 	public Bitmap getImage(String url) {
 		return imageMap.get(url);
 	}
-	
-	public void clearImage(){
+
+	public void clearImage() {
 		imageMap.clear();
 	}
 
@@ -90,61 +85,32 @@ public class ClickableListAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(final int position, View convertView, ViewGroup parent) {
-		
+
 		final HashMap<String, String> song = songsList.get(position);
 		imageURL = song.get(VariablesList.TAG_ALBUM_IMAGE);
 		View v = null;
-		
+
 		if (convertView != null) {
 			v = convertView;
 		} else {
 			v = inflater.inflate(R.layout.gridlayout_item, parent, false);
 		}
-		
-      	ImageView imageView = (ImageView) v.findViewById(R.id.icon_image);
+
+		ImageView imageView = (ImageView) v.findViewById(R.id.icon_image);
 		imageLoader.displayImage(imageURL, imageView, options);
 		imageView.setOnClickListener(new View.OnClickListener() {
 			// @Override
 			public void onClick(View v) {
-				albumUrl = new StringBuffer(resources
-						.getString(R.string.songsListURL));
+				albumUrl = resources.getString(R.string.songsListURL);
 				String albumIndex = song.get("id");
 				albumName = (song.get("name"));
 				imageURL = song.get(VariablesList.TAG_ALBUM_IMAGE);
 
-				SongsList albumList = new SongsList(imageURL, albumUrl,
-						albumName, albumIndex, resources);
-				Thread threadAlbumList = new Thread(albumList);
-				threadAlbumList.start();
-
-				synchronized (albumList) {
-					try {
-						albumList.wait();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				if (!NewMediaPlayer.mediaPlayer.isPlaying()) {
-					HashMap<String, String> playingSong = NewMediaPlayer.selectedSongs
-							.get(0);
-					if (playingSong != null) {
-						String url = playingSong.get("songUrl");
-						String songName = playingSong.get("songName");
-						if (songName != null) {
-							NewMediaPlayer.songTitleLabel.setText(albumName
-									+ " - " + songName);
-							NewMediaPlayer.songTitle.setText(albumName + "-"
-						
-									+ songName);
-						}
-						NewMediaPlayer.playSong(url);
-					
-					}
-				}
+				new SongsList(resources).execute(imageURL, albumUrl, albumName,
+						albumIndex);
 			}
 		});
 
-	
 		final TextView itemAlbumName = (TextView) v
 				.findViewById(R.id.icon_text);
 		itemAlbumName.setText(song.get("name"));
