57ab6eb4fdb45f0009c5d7fb2699f373c7407438
kanika23oct <devnull@localhost>
2013-08-14 22:58:16 -0700

    Formatted the playlist class

--- src/com/example/beamto/PlayList.java
+++ src/com/example/beamto/PlayList.java
@@ -1,23 +1,23 @@
 package com.example.beamto;
 
-import com.example.newplayer.R;
+import com.example.beamto.R;
 
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
-public class PlayList extends ListActivity{
-	
+public class PlayList extends ListActivity {
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.playlist);
-		System.out.println("***** Hello");
-		PlayListAdaptor songListAdapter = new PlayListAdaptor(this, NewMediaPlayer.selectedSongs);
+		PlayListAdaptor songListAdapter = new PlayListAdaptor(this,
+				NewMediaPlayer.selectedSongs);
 		ListView lv = getListView();
-		lv.setAdapter(songListAdapter);               
-		
+		lv.setAdapter(songListAdapter);
+
 	}
 
 }
