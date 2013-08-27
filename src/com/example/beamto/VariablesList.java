bef9ae94312f57c8336a3730874231ab895b6dcf
kanika23oct <devnull@localhost>
2013-08-27 00:36:19 -0700

    Added the new variables for artist tag

--- src/com/example/beamto/VariablesList.java
+++ src/com/example/beamto/VariablesList.java
@@ -7,6 +7,7 @@ public class VariablesList {
 	public static final String SOURCE_URL = "source_url";
 	public static final String TAG_LABEL_NAME = "label_name";
 	public static final String TAG_ALBUM_IMAGE = "coverart_small";
+	public static final String TAG_ARTIST_NAME = "artist";
 	public static final String ALBUM_JSON_PARAMETER = "album_id";
 	public static final String SONG_JSON_PARAMETER = "id";
 	public static final String SONG_NAME_PARAMETER = "songName";
@@ -16,6 +17,7 @@ public class VariablesList {
 	public static final String JSON_ALBUM_OBJECT = "albums";
 	public static final String JSON_SONG_OBJECT = "songs";
 	public static final String JSON_PAGE_OBJECT = "page";
+	public static final String NUMBER_OF_SONGS = "totalSongs";
 
 
 }
