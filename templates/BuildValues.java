1bad2b4b5f79857989492b2926df2fa25a06c38b
kanika23oct <devnull@localhost>
2013-08-29 20:32:45 -0700

    Added the BuildValues.java for the ant build debug and release mode

---
+++ templates/BuildValues.java
@@ -0,0 +1,10 @@
+package com.example.beamto;
+
+/**
+ * This class is automatically generated on build process. Change template in /templates/BuildValues.java
+ * or changes will be lost
+ */
+public class BuildValues {
+    public static final boolean IS_TEST = @app.test@;
+    public static final String BASE_URL = @app.base.url@;
+}
