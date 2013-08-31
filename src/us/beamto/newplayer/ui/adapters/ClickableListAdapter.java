149d1f4d6af9a213d64d6c45e2bddd8fe30b93a1
kanika23oct <devnull@localhost>
2013-08-30 22:45:34 -0700

    refractored the files to packages

---
+++ src/us/beamto/newplayer/ui/adapters/ClickableListAdapter.java
@@ -0,0 +1,151 @@
+package us.beamto.newplayer.ui.adapters;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+import us.beamto.newplayer.R;
+import us.beamto.newplayer.R.drawable;
+import us.beamto.newplayer.R.id;
+import us.beamto.newplayer.R.layout;
+import us.beamto.newplayer.common.VariablesList;
+import us.beamto.newplayer.ui.activites.SongListActivity;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ClickableListAdapter extends BaseAdapter {
+
+	private Context context;
+	private LayoutInflater inflater;
+	List<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+	String albumName = "";
+	private static Resources resources;
+	String albumUrl;
+	private String imageURL = "";
+	ImageView imageView;
+	DisplayImageOptions options;
+	ImageLoader imageLoader;
+
+	@SuppressWarnings("deprecation")
+	public ClickableListAdapter(Context context,
+			List<HashMap<String, String>> songList) {
+		inflater = LayoutInflater.from(context);
+		this.context = context;
+		this.songsList = songList;
+		resources = context.getResources();
+		imageLoader = ImageLoader.getInstance();
+		imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+		options = new DisplayImageOptions.Builder()
+				.showStubImage(R.drawable.ic_launcher)
+				.showImageForEmptyUri(R.drawable.ic_launcher).cacheOnDisc()
+				.cacheInMemory().build();
+
+	}
+
+	public void addToList(HashMap<String, String> song) {
+		songsList.add(song);
+	}
+
+	
+
+	@Override
+	public int getCount() {
+		return songsList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return songsList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent) {
+
+		final HashMap<String, String> song = songsList.get(position);
+		imageURL = song.get(VariablesList.TAG_ALBUM_IMAGE);
+		View v = null;
+
+		if (convertView != null) {
+			v = convertView;
+		} else {
+			v = inflater.inflate(R.layout.gridlayout_item, parent, false);
+		}
+
+		ImageView imageView = (ImageView) v.findViewById(R.id.icon_image);
+		imageLoader.displayImage(imageURL, imageView, options);
+		imageView.setOnClickListener(new View.OnClickListener() {
+			// @Override
+			public void onClick(View v) {
+				click(position);
+			}
+		});
+
+		final TextView itemAlbumName = (TextView) v
+				.findViewById(R.id.icon_text);
+		itemAlbumName.setText(song.get("name"));
+		itemAlbumName.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				click(position);
+			}
+		});
+		
+		final TextView itemArtistName = (TextView) v
+				.findViewById(R.id.album_artist);
+		itemArtistName.setText(song.get(VariablesList.TAG_ARTIST_NAME));
+		itemArtistName.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				click(position);
+			}
+		});
+		
+		final TextView itemTotalSongs = (TextView) v
+				.findViewById(R.id.total_songs);
+		itemTotalSongs.setText(song.get(VariablesList.NUMBER_OF_SONGS)+ " songs");
+		itemTotalSongs.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				click(position);
+			}
+		});
+
+		return v;
+	}
+
+	public void click(int position){
+		HashMap<String, String> song = songsList.get(position);
+		String songIndex = song.get("id");
+		String albumName = (song.get("name"));
+		String artistName = song.get(VariablesList.TAG_ARTIST_NAME);
+		String totalSongs = song.get(VariablesList.NUMBER_OF_SONGS);
+		System.out.println("%%%%%%% total Songs :"+ totalSongs);
+		Intent in = new Intent(context, SongListActivity.class);
+		in.putExtra("albumIndex", songIndex);
+		in.putExtra("albumName", albumName);
+		in.putExtra("albumImage",
+				song.get(VariablesList.TAG_ALBUM_IMAGE));
+		in.putExtra("artistName", artistName);
+		in.putExtra("totalSongs", totalSongs);
+		context.startActivity(in);
+		
+	}
+}
---
+++ src/us/beamto/newplayer/ui/adapters/SongListAdapter.java
@@ -0,0 +1,144 @@
+package us.beamto.newplayer.ui.adapters;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import us.beamto.newplayer.R;
+import us.beamto.newplayer.R.id;
+import us.beamto.newplayer.R.layout;
+import us.beamto.newplayer.R.string;
+import us.beamto.newplayer.api.JSONParser;
+import us.beamto.newplayer.common.VariablesList;
+import us.beamto.newplayer.ui.activites.NewMediaPlayerActivity;
+
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class SongListAdapter extends BaseAdapter {
+	ArrayList<HashMap<String, String>> songsList;
+	private Context context;
+	private LayoutInflater inflater;
+	private String albumName;
+	private static Resources resources;
+	HashMap<String, String> songs;
+	NewMediaPlayerActivity instance;
+
+	public SongListAdapter(Context context,
+			ArrayList<HashMap<String, String>> songList, String albumName) {
+		inflater = LayoutInflater.from(context);
+		this.context = context;
+		this.songsList = songList;
+		this.albumName = albumName;
+		resources = context.getResources();
+	}
+
+	@Override
+	public int getCount() {
+		return songsList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return songsList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		songs = songsList.get(position);
+		instance = NewMediaPlayerActivity.getActivity();
+		View v = null;
+		if (convertView != null)
+			v = convertView;
+		else
+			v = inflater.inflate(R.layout.songlist_item, parent, false);
+
+		TextView itemSongName = (TextView) v.findViewById(R.id.songTitle);
+		itemSongName.setText(songs.get(VariablesList.TAG_NAME));
+		itemSongName.setOnClickListener(new View.OnClickListener() {
+		
+			@Override
+			public void onClick(View view) {
+               String jsonSongURL = resources.getString(R.string.songURL);
+               	new PlaySong().execute(jsonSongURL,songsList.get(position).get(VariablesList.TAG_ID),songsList.get(position).get(VariablesList.TAG_NAME) );
+
+			}
+
+		});
+
+		return v;
+	}
+
+	private class PlaySong extends
+			AsyncTask<String, Void, HashMap<String, String>> {
+
+		final HashMap<String, String> song = new HashMap<String, String>();
+
+		@Override
+		protected HashMap<String, String> doInBackground(String... params) {
+			JSONParser jParser = new JSONParser();
+			try {
+				String jsonString = jParser.readJsonFromUrl(params[0],
+						VariablesList.TAG_ID, params[1]);
+				JSONObject jsonObject = new JSONObject(jsonString);
+				String songUrl = jsonObject.getString("url");
+				System.out.println(songUrl);
+				if (songUrl.startsWith("https://")) {
+					songUrl = songUrl.replaceFirst("https://", "http://");
+				}
+				song.put("songUrl", songUrl);
+				song.put("songName", params[2]);
+				song.put("albumName", albumName);
+				song.put("coverart_small", songs.get("AlbumImage"));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return song;
+
+		}
+
+		@Override
+		public void onPostExecute(HashMap<String, String> result) {
+
+			instance.addToSelectedList(result);
+			// NewMediaPlayer.selectedSongs.add(song);
+			if (!NewMediaPlayerActivity.mediaPlayer.isPlaying()) {
+			//	HashMap<String, String> playingSong = result;
+				if (result != null) {
+					String url = result.get("songUrl");
+					
+					String songName = result.get("songName");
+					if (songName != null) {
+						instance.setCurrentSongName(albumName + "-"+ songName);
+					}
+					instance.slidingDrawer.open();
+					instance.playSong(url);
+					((Activity)context).finish();
+				}
+			}
+		}
+
+	}
+
+}
