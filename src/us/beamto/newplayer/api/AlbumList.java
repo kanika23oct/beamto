d3c9086250864d65d9010ad5fa2e0ee0bc3e22d5
kanika23oct <kanika23oct@gmail.com>
2013-09-27 00:35:09 -0700

    Updated the AlbumList

--- src/us/beamto/newplayer/api/AlbumList.java
+++ src/us/beamto/newplayer/api/AlbumList.java
@@ -13,56 +13,71 @@ import android.util.Log;
 
 import org.json.*;
 
-
 import us.beamto.newplayer.common.VariablesList;
 import us.beamto.newplayer.ui.activites.NewMediaPlayerActivity;
 
 public class AlbumList {
-	
-	String albumUrl ;
-	
-	public AlbumList(String albumUrl){
-		this.albumUrl=albumUrl;
+
+	String albumUrl;
+
+	public AlbumList(String albumUrl) {
+		this.albumUrl = albumUrl;
 	}
-	
+
 	private ArrayList<HashMap<String, String>> albumList = new ArrayList<HashMap<String, String>>();
 
-	public ArrayList<HashMap<String, String>> songList(String url,String parameter,String value) {
+	public ArrayList<HashMap<String, String>> songList(String url,
+			String parameter, String value) {
 		try {
 			JSONParser jParser = new JSONParser();
-			String jsonString = jParser.readJsonFromUrl(url.toString(),parameter,value);
+			String jsonString = jParser.readJsonFromUrl(url.toString(),
+					parameter, value);
 			JSONObject jsonObject = new JSONObject(jsonString);
-			JSONArray songs = jsonObject.getJSONArray(VariablesList.JSON_ALBUM_OBJECT);
+			JSONArray songs = jsonObject
+					.getJSONArray(VariablesList.JSON_ALBUM_OBJECT);
+			System.out.println("**** Size: " + songs.length());
 			for (int i = 0; i < songs.length(); i++) {
 				HashMap<String, String> album = new HashMap<String, String>();
 				JSONObject albumDetails = songs.getJSONObject(i);
 				String id = albumDetails.getString(VariablesList.TAG_ID);
+				String slug = albumDetails.getString(VariablesList.TAG_SLUG);
 				String name = albumDetails.getString(VariablesList.TAG_NAME);
-				String labelName = albumDetails.getString(VariablesList.TAG_LABEL_NAME);
-				String albumImage = albumDetails.getString(VariablesList.TAG_ALBUM_IMAGE);
-				String artistName = albumDetails.getString(VariablesList.TAG_ARTIST_NAME);
+				String labelName = albumDetails
+						.getString(VariablesList.TAG_LABEL_NAME);
+				String albumImage = albumDetails
+						.getString(VariablesList.TAG_ALBUM_IMAGE);
+				String artistName = albumDetails
+						.getString(VariablesList.TAG_ARTIST_NAME);
+
+				String songJsonString = jParser.readJsonFromUrl(
+						albumUrl.toString(),
+						VariablesList.ALBUM_JSON_PARAMETER, slug);
+					JSONObject songjsonObject = new JSONObject(songJsonString);
+						NewMediaPlayerActivity
+							.getActivity()
+							.setAlbumJsonArray(
+									id + ";" + name,
+									songjsonObject
+											.getJSONArray(VariablesList.JSON_SONG_OBJECT));
+					album.put(VariablesList.TAG_ID, id);
+					album.put(VariablesList.TAG_NAME, name);
+					album.put(VariablesList.TAG_LABEL_NAME, labelName);
+					album.put(VariablesList.TAG_ALBUM_IMAGE, albumImage);
+					album.put(VariablesList.TAG_ARTIST_NAME, artistName);
+					String size = ""
+							+ songjsonObject.getJSONArray(
+									VariablesList.JSON_SONG_OBJECT).length();
+					album.put(VariablesList.NUMBER_OF_SONGS, size);
+					albumList.add(album);
 				
-				String songJsonString = jParser.readJsonFromUrl(albumUrl.toString(),VariablesList.ALBUM_JSON_PARAMETER,id);
-				JSONObject songjsonObject = new JSONObject(songJsonString);
-				NewMediaPlayerActivity.getActivity().setAlbumJsonArray(id+";"+name,songjsonObject.getJSONArray(VariablesList.JSON_SONG_OBJECT));
-				album.put(VariablesList.TAG_ID, id);
-				album.put(VariablesList.TAG_NAME, name);
-				album.put(VariablesList.TAG_LABEL_NAME, labelName);
-				album.put(VariablesList.TAG_ALBUM_IMAGE, albumImage);
-				album.put(VariablesList.TAG_ARTIST_NAME, artistName);
-				String size = ""+songjsonObject.getJSONArray(VariablesList.JSON_SONG_OBJECT).length();
-				album.put(VariablesList.NUMBER_OF_SONGS,size );
-				albumList.add(album);
 			}
 
-		
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return albumList;
 	}
 
-	
 	public static Bitmap LoadImagetoGridView(String url) {
 		String image = url;
 		Bitmap mIcon11 = null;
@@ -71,9 +86,9 @@ public class AlbumList {
 			in = new java.net.URL(image).openStream();
 			BitmapFactory.Options options = new BitmapFactory.Options();
 			options.inSampleSize = 2;
-			
+
 			mIcon11 = BitmapFactory.decodeStream(in, null, options);
-			
+
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -83,7 +98,5 @@ public class AlbumList {
 		}
 		return mIcon11;
 	}
-
-	
 
 }
