299126bf029dbbdaade24f6f9609202646bffd95
kanika23oct <kanika23oct@gmail.com>
2013-09-26 23:14:09 -0700

    Updated the project files

--- deploy-debug.bat
+++ deploy-debug.bat
@@ -1,4 +1,4 @@
-adb shell pm uninstall com.example.newplayer
-adb -d install bin\BeamTo-debug.apk
+adb shell pm uninstall us.beamto.newplayer
+adb -d install bin/BeamTo-debug.apk
 adb shell am start -n us.beamto.newplayer/us.beamto.newplayer.ui.activites.NewMediaPlayerActivity
 pause
--- deploy-debug.sh
+++ deploy-debug.sh
@@ -1,4 +1,6 @@
-adb shell pm uninstall com.example.newplayer
-adb -d install bin/BeamTo-debug.apk
+#!/bin/bash
+
+adb shell pm uninstall us.beamto.newplayer
+adb -s $1 install bin/BeamTo-debug.apk
 adb shell am start -n us.beamto.newplayer/us.beamto.newplayer.ui.activites.NewMediaPlayerActivity
 pause
