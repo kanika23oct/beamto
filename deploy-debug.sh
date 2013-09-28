a10040a81ba661ae1b603eb54523aa6245baf8cd
kanika23oct <kanika23oct@gmail.com>
2013-09-28 00:43:14 -0700

    Exited the deploy script build the apk by default and exit the script in case of failure

--- deploy-debug.sh
+++ deploy-debug.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
+#!/bin/bash -e
+set -e
+if [ $# -eq 0 ] 
+ then
+  sh build-debug.sh
+fi  
 
-adb shell pm uninstall us.beamto.newplayer
-adb -s $1 install bin/BeamTo-debug.apk
+output=`adb shell pm uninstall us.beamto.newplayer`
+echo $output
+if [[ $output == *"Failure"* ]]; then
+   echo "Exiting the deploy script"
+   exit 1
+fi
+adb -d install bin/BeamTo-debug.apk
 adb shell am start -n us.beamto.newplayer/us.beamto.newplayer.ui.activites.NewMediaPlayerActivity
 pause
