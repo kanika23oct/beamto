a76684485e1b05fdf33f0943497d1e2c64a863c9
kanika23oct <kanika23oct@gmail.com>
2013-09-28 00:44:40 -0700

    change the script to create a crlease apk by default

--- deploy-release.sh
+++ deploy-release.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
+if [ $# eq 0 ]
+  then
+   sh build-release.sh
+fi
+
 adb shell pm uninstall com.example.newplayer
+
 adb -d install bin/BeamTo-release.apk
 adb shell am start -n us.beamto.newplayer/us.beamto.newplayer.ui.activites.NewMediaPlayerActivity
 pause
