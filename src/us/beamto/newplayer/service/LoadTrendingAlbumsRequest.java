f83d62ca9685b2bfc35b118d78417893cde2f5d7
kanika23oct <kanika23oct@gmail.com>
2013-12-21 23:37:24 -0800

    Updated the parseJsonResponse function to add the albums to the list

--- src/us/beamto/newplayer/service/LoadTrendingAlbumsRequest.java
+++ src/us/beamto/newplayer/service/LoadTrendingAlbumsRequest.java
@@ -1,5 +1,6 @@
 package us.beamto.newplayer.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
@@ -11,15 +12,17 @@ import org.json.JSONObject;
 
 import us.beamto.newplayer.api.ApiRequestResult;
 import us.beamto.newplayer.api.BaseApiRequest;
+import us.beamto.newplayer.api.JSONParser;
 import us.beamto.newplayer.api.PostApiRequest;
 import us.beamto.newplayer.common.BuildValues;
 import us.beamto.newplayer.common.VariablesList;
+import us.beamto.newplayer.ui.activites.NewMediaPlayerActivity;
 import us.beamto.newplayer.ui.activites.SongListActivity;
 import us.beamto.newplayer.ui.activites.Subscriber;
 
 public class LoadTrendingAlbumsRequest extends PostApiRequest implements Observer{
 
-String loadAlbumURL = "";
+ArrayList<HashMap<String, String>> albumList = new ArrayList<HashMap<String, String>>();
 	
     public LoadTrendingAlbumsRequest(HttpClient httpClient) {
     	super(httpClient);
@@ -42,35 +45,79 @@ String loadAlbumURL = "";
         resetParams();
 
         addParam(parameter[0], params[0]);
-        loadAlbumURL = params[1];
-        return this;
+              return this;
     }
 
 
     @Override
-    protected void parseJsonResponse(ApiRequestResult requestResult, JSONObject songResultObject) throws JSONException {
-        super.parseJsonResponse(requestResult, songResultObject);
-        if (requestResult.isStatusSuccessful()) {
-            try {
-            	JSONArray songs = songResultObject
-						.getJSONArray(VariablesList.JSON_SONG_OBJECT);
-            	for (int i = 0; i < songs.length(); i++) {
-					HashMap<String, String> song = new HashMap<String, String>();
-					JSONObject songDetails = songs.getJSONObject(i);
-					String id = songDetails.getString(VariablesList.TAG_ID);
-					String name = songDetails.getString(VariablesList.TAG_NAME);
-					song.put(VariablesList.TAG_ID, id);
-					song.put(VariablesList.TAG_NAME, name);
-					song.put("AlbumImage", loadAlbumURL);
-					SongListActivity.addToSongsList(song);
+	protected void parseJsonResponse(ApiRequestResult requestResult,
+			JSONObject albumResultObject) throws JSONException {
+		super.parseJsonResponse(requestResult, albumResultObject);
+		NewMediaPlayerActivity instance = NewMediaPlayerActivity.getActivity();
+		JSONParser jParser = new JSONParser();
+		if (requestResult.isStatusSuccessful()) {
+			try {
+				JSONArray albums = albumResultObject
+						.getJSONArray(VariablesList.JSON_ALBUM_OBJECT);
+				for (int i = 0; i < albums.length(); i++) {
+					HashMap<String, String> album = new HashMap<String, String>();
+					JSONObject albumDetails = albums.getJSONObject(i);
+					String id = albumDetails.getString(VariablesList.TAG_ID);
+					String slug = albumDetails
+							.getString(VariablesList.TAG_SLUG);
+					String name = albumDetails
+							.getString(VariablesList.TAG_NAME);
+					String labelName = albumDetails
+							.getString(VariablesList.TAG_LABEL_NAME);
+					String albumImage = albumDetails
+							.getString(VariablesList.TAG_ALBUM_IMAGE);
+					String artistName = albumDetails
+							.getString(VariablesList.TAG_ARTIST_NAME);
+					String songJsonString = jParser
+							.readJsonFromUrl(BuildValues.BASE_URL
+									+ VariablesList.SONGS_LIST_URL,
+									VariablesList.ALBUM_JSON_PARAMETER, slug);
+					JSONObject songjsonObject = new JSONObject(songJsonString);
+					NewMediaPlayerActivity
+							.getActivity()
+							.setAlbumJsonArray(
+									id + ";" + name,
+									songjsonObject
+											.getJSONArray(VariablesList.JSON_SONG_OBJECT));
+
+					album.put(VariablesList.TAG_ID, id);
+					album.put(VariablesList.TAG_SLUG, slug);
+					album.put(VariablesList.TAG_NAME, name);
+					album.put(VariablesList.TAG_LABEL_NAME, labelName);
+					album.put(VariablesList.TAG_ALBUM_IMAGE, albumImage);
+					album.put(VariablesList.TAG_ARTIST_NAME, artistName);
+					String size = ""
+							+ songjsonObject.getJSONArray(
+									VariablesList.JSON_SONG_OBJECT).length();
+					album.put(VariablesList.NUMBER_OF_SONGS, size);
+					albumList.add(album);
+
+				}
+               
+				System.out.println("Album Size :"+albumList.size());
+				if (albumList.size() == 0) {
+					instance.setLastPage(true);
+					instance.setLoading(false);
+
+				} else {
+					instance.setNewList(albumList);
+					instance.setLoading(false);
+					instance.appendToAdaptor();
+					
 				}
 				
-		     } catch (Exception e) {
-                requestResult.resultObject = "";
-            }
-        }
-    }
-
+			//	ClickableListAdapter adapter = instance.getClickableListAdapter();
+			//	adapter.notifyDataSetChanged();
+			} catch (Exception e) {
+				requestResult.resultObject = "";
+			}
+		}
+	}
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		
