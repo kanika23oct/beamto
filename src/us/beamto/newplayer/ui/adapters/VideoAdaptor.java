513bd0a7f8084244bdd0cbe9594a5ad4cc93cf72
kanika23oct <kanika23oct@gmail.com>
2014-02-25 23:17:22 -0800

    Added the logic for displaying the image in VideoAdaptor.java

--- src/us/beamto/newplayer/ui/adapters/VideoAdaptor.java
+++ src/us/beamto/newplayer/ui/adapters/VideoAdaptor.java
@@ -3,43 +3,77 @@ package us.beamto.newplayer.ui.adapters;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import us.beamto.newplayer.R;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
 import android.content.Context;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+
+import us.beamto.newplayer.common.VariablesList;
 
 public class VideoAdaptor extends BaseAdapter{
 	private Context context;
-	private ArrayList<HashMap<String, String>> videoPlayList;
+	private ArrayList<HashMap<String, String>> videoPlayLists = new ArrayList<HashMap<String, String>> ();
+	private LayoutInflater inflater;
+	private static Resources resources;
+	ImageView imageView;
+	DisplayImageOptions options;
+	ImageLoader imageLoader;
+    String imageURL = "";
 	
 	public VideoAdaptor(Context context,ArrayList<HashMap<String, String>> videoPlayList)
 	{
 		this.context = context;
-		this.videoPlayList = videoPlayList;
+		inflater = LayoutInflater.from(context);
+		this.videoPlayLists = videoPlayList;
+		resources = context.getResources();
+		imageLoader = ImageLoader.getInstance();
+		imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+		options = new DisplayImageOptions.Builder()
+				.showStubImage(R.drawable.ic_launcher)
+				.showImageForEmptyUri(R.drawable.ic_launcher).cacheOnDisc()
+				.cacheInMemory().build();
 	}
 
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return videoPlayLists.size();
 	}
 
 	@Override
-	public Object getItem(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object getItem(int position) {
+		return videoPlayLists.get(position);
 	}
 
 	@Override
-	public long getItemId(int arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+	public long getItemId(int position) {
+		return position;
 	}
 
 	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
-		// TODO Auto-generated method stub
-		return null;
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		HashMap<String,String> videoPlayList = videoPlayLists.get(position);
+		View v = null;
+		if(convertView != null)
+			v = convertView;
+		else
+			v = inflater.inflate(R.layout.li_clickable_list, parent, false);
+		
+		ImageView imageView = (ImageView)v.findViewById(R.id.icon_image);
+		imageURL = videoPlayList.get(VariablesList.TAG_ALBUM_IMAGE);
+		imageLoader.displayImage(imageURL, imageView,options);
+		
+		
+		return v;
 	}
 
 }
